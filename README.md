84e61ef62042ef6762061b741d0021dcd06aa7a7
gxiaobang <ninezero.guo@gmail.com>
2017-05-03 17:17:00 +0800

    post init

---
+++ README.md
@@ -0,0 +1,12 @@
+## nodejs 服务器
+
+
+### 安装
+```bash
+yarn install
+```
+
+### 运行
+```bash
+yarn start
+```
---
+++ index.js
@@ -0,0 +1,41 @@
+/**
+ * 请求代理
+ */
+
+const express = require('express');
+const proxy = require('http-proxy-middleware');
+// const opn = require('opn');
+
+const app = express();
+const path = require('path');
+const { version, host, port, distPath, api } = require('./config');
+
+const sitePath = path.resolve(distPath, version);
+
+
+// 接口代理
+for (let key in api.dev) {
+  app.use(`/${key}`, proxy({
+    target: api.dev[ key ],
+    changeOrigin: true,
+    pathRewrite: {
+      [`^/${key}`]: ''
+    }
+  }));
+}
+
+// 静态文件
+app.use(express.static(sitePath));
+
+// 重定向到主页
+app.get('*', (req, res) => {
+  res.sendFile(`${sitePath}/index.html`);
+});
+
+
+// 监听port端口
+app.listen(port, host, (err) => {
+  if (err) throw err;
+  console.log(`Listening at http://localhost:${port}`);
+  // opn(`http://localhost:${port}`);
+});
