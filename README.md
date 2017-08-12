f5bdd720c26187a25c9e578617bba6ace389e46f
gxiaobang <gxiaobang@163.com>
2017-08-12 13:55:04 +0800

    配置修改

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Web服务器
 
 ## 项目构建
+
 ### 安装
 ```bash
 npm install
--- start.js
+++ start.js
@@ -8,43 +8,31 @@ const proxy = require('http-proxy-middleware');
 
 const app = express();
 const path = require('path');
-const { version, host, port, distPath, api } = require('./config');
+const { port, distPath, api } = require('./config');
 
 const argv = require('yargs')
-  .option('ver', {
-    default: version
-  })
   .option('refer', {
     default: 'dev'
   })
   .argv;
 
-const sitePath = path.resolve(distPath, argv.ver);
-
 // 接口代理
 for (let key in api[argv.refer]) {
-  app.use(`/proxy/${key}`, proxy({
+  app.use('/' + key, proxy({
     target: api[argv.refer][ key ],
-    changeOrigin: true,
-    pathRewrite: {
-      '^/proxy': ''
-    }
+    changeOrigin: true
   }));
 }
 
 // 静态文件
-app.use(express.static(sitePath));
+app.use(express.static(distPath));
 
-// 重定向到主页
-app.get('*', (req, res) => {
-  // console.log(req.query)
-  let { lang = 'zh-cn' } = req.query;
-  res.sendFile(`${sitePath}/index_${lang}.html`);
-});
+// handle fallback for HTML5 history API
+// app.use(require('connect-history-api-fallback')())
 
 
 // 监听port端口
-app.listen(port, host, (err) => {
+app.listen(port, '0.0.0.0', (err) => {
   if (err) throw err;
   console.log(`Listening at http://localhost:${port}`);
   // opn(`http://localhost:${port}`);
