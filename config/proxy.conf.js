3fdcc333add0bba0febd32aa218e49d77816f735
gxiaobang <gxiaobang@163.com>
2017-08-13 09:32:24 +0800

    优化参数配置

---
+++ config/proxy.conf.js
@@ -0,0 +1,17 @@
+/**
+ * 接口代理
+ */
+
+const proxyTable = {
+	// 开发环境
+	dev: {
+	},
+	// 测试环境
+	test: {
+	},
+	// 生产环境
+	prod: {
+	}
+};
+
+module.exports = proxyTable;
--- start.js
+++ start.js
@@ -1,39 +1,51 @@
 /**
- * 前端服务器
+ * 静态服务器
  */
 
 const express = require('express');
 const proxy = require('http-proxy-middleware');
-// const opn = require('opn');
+const opn = require('opn');
 
 const app = express();
 const path = require('path');
-const { port, distPath, api } = require('./config');
 
 const argv = require('yargs')
-  .option('refer', {
+  .option('proxy', {
     default: 'dev'
+  })
+  .option('port', {
+    alias: 'p',
+    default: 8080
+  })
+  .option('open', {
+    alias: 'o',
+    default: false
   })
   .argv;
 
+
 // 接口代理
-for (let key in api[argv.refer]) {
+const proxyTable = require('./config/proxy.conf');
+for (let key in proxyTable[argv.proxy]) {
   app.use('/' + key, proxy({
-    target: api[argv.refer][ key ],
+    target: api[argv.proxy][ key ],
     changeOrigin: true
   }));
 }
 
 // 静态文件
-app.use(express.static(distPath));
+app.use(express.static('./dist'));
 
 // handle fallback for HTML5 history API
-// app.use(require('connect-history-api-fallback')())
+// app.use(require('connect-history-api-fallback'))
 
 
 // 监听port端口
-app.listen(port, '0.0.0.0', (err) => {
+app.listen(argv.port, '0.0.0.0', (err) => {
   if (err) throw err;
-  console.log(`Listening at http://localhost:${port}`);
-  // opn(`http://localhost:${port}`);
+  console.log(`> Listening at http://localhost:${argv.port}`);
+
+  if (argv.open) {
+    opn(`http://localhost:${argv.port}`);
+  }
 });
